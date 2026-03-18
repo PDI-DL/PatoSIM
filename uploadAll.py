e55829bd7d42ab5df2a77d78cb7d7d1da5939cfe
breno-xp <brexp14@gmail.com>
2026-03-18 19:21:00 -0300

    Add pasta vazia para o script uploadAll.py importar todos assets de PDI-DL/PDI_3DUW do Hugging Face

---
+++ uploadAll.py
@@ -0,0 +1,21 @@
+# import huggingface_hub
+# print(huggingface_hub.__version__)
+
+import os
+from huggingface_hub import HfApi
+
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+api = HfApi()
+
+local_folder_path = f"{os.getcwd()}/modelos_os"
+hf_repo_id = "PDI-DL/PDI_3DUW"
+
+print(f"Starting upload for {hf_repo_id}...")
+
+api.upload_large_folder(
+    folder_path=local_folder_path,
+    repo_id=hf_repo_id,
+    repo_type="dataset",  
+)
+
+print("Upload complete!")
