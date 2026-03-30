fe0e250bb1600c11e9b127a916c04d88399e2587
breno-xp <brexp14@gmail.com>
2026-03-30 17:28:00 -0300

    Incremento na lógica dos Scripts de download e upload de modelos do HuggingFace

--- scripts/assets/downloadAll.py
+++ scripts/assets/downloadAll.py
@@ -1,5 +1,7 @@
 import os
-from huggingface_hub import snapshot_download
+import shutil
+from huggingface_hub import HfApi, snapshot_download
+from huggingface_hub.utils import LocalTokenNotFoundError, RepositoryNotFoundError
 
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
@@ -7,20 +9,45 @@ hf_repo_id = "PDI-DL/PDI_3DUW"
 
 current = __file__
 
-for i in range(3):
-    # print(current)
-    local_download_path = os.path.dirname(current)
-    current = local_download_path
-else:
-    local_download_path += "/assets/models/"
+api = HfApi()
 
-print(f"### Starting download for {hf_repo_id}...")
+try:
+    user = api.whoami()["name"]
+    print(f"### Logged as {user}")
+    try:
+        api.repo_info(repo_id=hf_repo_id, repo_type="dataset")
+        print(f"### {hf_repo_id} is a valid Repo ID")
+        try:
+            for i in range(3):
+                # print(current)
+                local_download_path = os.path.dirname(current)
+                current = local_download_path
+            else:
+                local_download_path += "/assets/models/"
 
 
-download_path = snapshot_download(
-    repo_id=hf_repo_id,
-    repo_type="dataset",
-    local_dir=local_download_path,
-)
+            if os.path.exists(local_download_path):
+                print(f"##### Directory already exists at: {local_download_path}. Removing it...")
+                shutil.rmtree(local_download_path)
+                print("##### Directory removed.")
 
-print(f"### Download complete! Files saved to: {download_path}")
+            print(f"### Starting download for {hf_repo_id}...")
+
+
+            download_path = snapshot_download(
+                repo_id=hf_repo_id,
+                repo_type="dataset",
+                local_dir=local_download_path,
+            )
+
+            print(f"##### Download complete! Files saved to: {download_path}")
+        except Exception as e:
+            print(f"##### Error: download failed ###\n {e}")
+    except RepositoryNotFoundError:
+        print(f"##### Error: invalid Repo ID ###")
+    except Exception as e:
+        print(f"##### Error: Repo ID ### \n{e}")
+except LocalTokenNotFoundError as e:
+    print(f"##### Error: user is not logged in or authentication failed ### \n{e}")
+except Exception as e:
+    print(f"##### Error: not logged in or token is invalid: ### \n{e}")
--- scripts/assets/uploadAll.py
+++ scripts/assets/uploadAll.py
@@ -1,29 +1,51 @@
-# import huggingface_hub
-# print(huggingface_hub.__version__)
-
 import os
+import shutil
 from huggingface_hub import HfApi
+from huggingface_hub.utils import LocalTokenNotFoundError, RepositoryNotFoundError
 
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
-api = HfApi()
-
-current = __file__
-
-for i in range(3):
-    # print(current)
-    local_folder_path = os.path.dirname(current)
-    current = local_folder_path
-else:
-    local_folder_path += "/assets/models/"
 
 hf_repo_id = "PDI-DL/PDI_3DUW"
 
-print(f"### Starting upload for {hf_repo_id}...")
+current = __file__
 
-api.upload_large_folder(
-    folder_path=local_folder_path,
-    repo_id=hf_repo_id,
-    repo_type="dataset",  
-)
+api = HfApi()
 
-print("### Upload complete!")
+try:
+    user = api.whoami()["name"]
+    print(f"### Logged as {user}")
+    try:
+        api.repo_info(repo_id=hf_repo_id, repo_type="dataset")
+        print(f"### {hf_repo_id} is a valid Repo ID")
+        try:
+                for i in range(3):
+                    # print(current)
+                    local_folder_path = os.path.dirname(current)
+                    current = local_folder_path
+                else:
+                    local_folder_path += "/assets/models/"
+
+                if not os.path.exists(local_folder_path):
+                    print(f"Directory does not exist at: {local_folder_path}... Stopping upload")
+                    raise Exception(f"No directory at {local_folder_path}")
+
+                print(f"### Starting upload for {hf_repo_id}...")
+
+                api.upload_large_folder(
+                    folder_path=local_folder_path,
+                    repo_id=hf_repo_id,
+                    repo_type="dataset",  
+                )
+
+                print(f"##### Upload complete! Files saved to: {hf_repo_id}")
+        except Exception as e:
+            print(f"##### Error: upload failed ###\n {e}")
+    except RepositoryNotFoundError:
+        print(f"##### Error: invalid Repo ID ###")
+    except Exception as e:
+        print(f"##### Error: Repo ID ### \n{e}")
+except LocalTokenNotFoundError as e:
+    print(f"##### Error: user is not logged in or authentication failed ### \n{e}")
+except Exception as e:
+    print(f"##### Error: not logged in or token is invalid: ### \n{e}")
+
